684a0bf2717d0662aa1089ba9a64485c3921252d
Henry Skalla <hcskalla@gmail.com>
2024-04-30 09:53:43 -0500

    initializing project

---
+++ README.md
@@ -0,0 +1,4 @@
+# Algorithms Final Project: Dithering
+## Authors: Henry Skalla, Jay Lebakken, James McConnell
+
+TODO: Write README.
---
+++ src/dithering/App.java
@@ -0,0 +1,10 @@
+package dithering;
+
+/**
+ * TODO: add javadoc
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("\n\nhello world :)\n\n");
+    }
+}
